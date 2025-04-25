dbaafa50056f2646d3fbe7f4800696dda4251efa
Ricky <rich.ries@gmail.com>
2025-04-25 14:57:03 -0400

    add LSPs: clangd, ts_ls, pyright

--- lua/kickstart/plugins/lsp-config.lua
+++ lua/kickstart/plugins/lsp-config.lua
@@ -16,26 +16,7 @@ return {
     'saghen/blink.cmp',
   },
   config = function()
-    -- Brief aside: **What is LSP?**
-    --
-    -- LSP is an initialism you've probably heard, but might not understand what it is.
-    --
-    -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-    -- and language tooling communicate in a standardized fashion.
-    --
-    -- In general, you have a "server" which is some tool built to understand a particular
-    -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-    -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-    -- processes that communicate with some "client" - in this case, Neovim!
-    --
-    -- LSP provides Neovim with features like:
-    --  - Go to definition
-    --  - Find references
-    --  - Autocompletion
-    --  - Symbol Search
-    --  - and more!
-    --
-    -- Thus, Language Servers are external tools that must be installed separately from
+    -- Language Servers are external tools that must be installed separately from
     -- Neovim. This is where `mason` and related plugins come into play.
     --
     -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
@@ -194,9 +175,9 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
+      clangd = {},
       -- gopls = {},
-      -- pyright = {},
+      pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
@@ -204,8 +185,7 @@ return {
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-      --
+      ts_ls = {},
 
       lua_ls = {
         -- cmd = { ... },
