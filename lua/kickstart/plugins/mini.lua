16c7845005957afce47c43135f37f735cee42b06
Ricky <rich.ries@gmail.com>
2025-04-30 17:18:09 -0400

    remap s to no-op to prevent misfires on slow mini.surround queries

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -14,8 +14,11 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
 
+    -- Disable standalone 's' (to prevent substitution)
+    vim.keymap.set('n', 's', '<Nop>', { noremap = true, silent = true })
+
+    require('mini.surround').setup()
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
